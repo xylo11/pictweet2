59ed276ca72b85b4c2964e6203a5ae48dc70ef2a
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-08 18:18:44 +0900

    ツイートの削除機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,25 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 end
